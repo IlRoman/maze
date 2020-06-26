5aac619979760514364eda71c964b490de9ae0ba
“ilroman” <il.roman@ukr.net>
2020-06-26 09:18:13 +0300

    Updates

---
+++ precache-manifest.74831b7f041f5173120a843c920f3fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6503ad3ef9abe1abb658567bd524b5db",
+    "url": "/maze/index.html"
+  },
+  {
+    "revision": "f0ea0247df0d81f3a437",
+    "url": "/maze/static/css/main.8f394e25.chunk.css"
+  },
+  {
+    "revision": "614c56560f8924f953cb",
+    "url": "/maze/static/js/2.51d07da9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/maze/static/js/2.51d07da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ea0247df0d81f3a437",
+    "url": "/maze/static/js/main.e4797a09.chunk.js"
+  },
+  {
+    "revision": "d20dd580d7e930348d38",
+    "url": "/maze/static/js/runtime-main.b63a93ea.js"
+  }
+]);
